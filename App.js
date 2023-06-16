7bee66c46f83c32d4a9a4ccae80acc286dea436f
pratiksha garg <pratiksha.garg@nagarro.com>
2023-06-16 19:12:28 +0530

    episode -01

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading1 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Heading 1"),
+    React.createElement("h2", {}, "Heading 2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Heading 1"),
+    React.createElement("h2", {}, "Heading 2"),
+  ]),
+]);
+
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(heading1);
